de2d7cb5ce48f3ba60f92d12dc192129451f75f4
Timm Stokke <timm@stokke.me>
2015-03-09 12:52:57 +0100

    Links to better navigate routes, computed properties, etc

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,9 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   first_name: DS.attr('string'),
   last_name: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
-  total_articles: DS.attr('number')
+  total_articles: DS.attr('number'),
+  full_name: Ember.computed('first_name', 'last_name', function() {
+    return this.get('first_name') + ' ' + this.get('last_name');
+  })
 });
