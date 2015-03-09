688a820a4a49652ffa2e75bcac5308634365e921
Timm Stokke <timm@stokke.me>
2015-03-09 11:34:23 +0100

    non-working save, but otherwise good

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,10 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.first_name',
+    'model.last_name',
+    'model.twitter',
+    function() {
+
+      console.log(this.get('model.email'));
+
+      return !Ember.isEmpty(this.get('model.email')) &&
+        !Ember.isEmpty(this.get('model.first_name')) &&
+        !Ember.isEmpty(this.get('model.last_name')) &&
+        !Ember.isEmpty(this.get('model.twitter'));
+    }
+  ),
   actions: {
     save: function() {
-      console.log('+- save action i friends new controller');
-      return true; // make it continue bubbling
+      if (this.get('isValid')) {
+        alert('isValid');
+        var _this = this;
+        this.get('model').save().then(function(friend) {
+          _this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill out all the fields');
+      }
+
+      return false; // do not bubble up to routes
     },
     cancel: function() {
       console.log('+- cancel action i friends new controller');
