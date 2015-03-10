59bb1736d7187529a2f879d9682cbddcef9a7edb
Timm Stokke <timm@stokke.me>
2015-03-10 23:09:58 +0100

    move route -> controller + created articles index controller

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // queryParams: ['show'], // Uncaught Error: Assertion Failed: You passed in `["show"]` as the value for `queryParams` but `queryParams` cannot be an Array
-  // ref loc 1627 in book
-  possibleStates: ['borrowed', 'returned'],
   model: function() {
     return this.modelFor('friends/show').get('articles');
+  },
+  actions: {
+    save: function(model) {
+      model.save();
+      return false; // no bubblies!
+    }
   }
 });
