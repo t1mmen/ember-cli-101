04715527b2e0eb129022f375b44eb4aed7600f38
Timm Stokke <timm@stokke.me>
2015-03-10 12:31:56 +0100

    43% in book

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 //export default DS.RESTAdapter.extend({
 
 export default DS.ActiveModelAdapter.extend({
-  namespace: 'api',
+  namespace: 'api/v2',
   host: 'http://api.ember-cli-101.com',
 });
