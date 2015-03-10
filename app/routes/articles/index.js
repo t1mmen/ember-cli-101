09cfd3295109e862fcdba4e329c691b4d03d582b
Timm Stokke <timm@stokke.me>
2015-03-10 22:46:26 +0100

    1.2 to 1.10 update complete
    queryParams is giving me issues though, looks right according to
    http://emberjs.com/guides/routing/query-params/ ?

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: ['show'],
+  // queryParams: ['show'], // Uncaught Error: Assertion Failed: You passed in `["show"]` as the value for `queryParams` but `queryParams` cannot be an Array
+  // ref loc 1627 in book
   possibleStates: ['borrowed', 'returned'],
   model: function() {
     return this.modelFor('friends/show').get('articles');
