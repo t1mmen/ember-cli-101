406fe327f23c525bd3f09d6fb6f7ccf31be9bd2f
Timm Stokke <timm@stokke.me>
2015-03-10 23:32:20 +0100

    52%, autosave state

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -4,6 +4,18 @@ export default Ember.Component.extend({
   tagName: 'tr',
   article: null, // passed in
   articleStates: null, //passed in
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
+      this.sendAction('save', article);
+    }
+  },
+  stateChanged: function() {
+    var article = this.get('article');
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  }.on('init').observes('article.state'),
   actions: {
     saveArticle: function(article) {
       this.sendAction('save', article); //
