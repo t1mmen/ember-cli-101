92ac33af2dcc5aab4e937d4f86f74719324102a2
Timm Stokke <timm@stokke.me>
2015-03-17 11:39:38 +0100

    75% API v4, force reload of users' articles

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 //export default DS.RESTAdapter.extend({
 
 export default DS.ActiveModelAdapter.extend({
-  namespace: 'api/v3',
+  namespace: 'api/v4',
   host: 'http://api.ember-cli-101.com',
   coalesceFindRequests:true, // Combine finds to async relationships
   // namespace: 'cakephp-ember-backend/myapp',
--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.modelFor('friends.show').get('articles');
+    var articles = this.modelFor('friends.show').get('articles');
+
+    // Force reload of articles if they've been fetched before
+    // for this friend.
+    if (articles.get('isFulfilled')) {
+      articles.reload();
+    }
+
+    return articles;
+
   },
   actions: {
     save: function(model) {
