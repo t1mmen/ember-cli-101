88e3d8e4d292b2a38d204b4af8ae78cd0168389f
Timm Stokke <timm@stokke.me>
2015-03-17 13:08:16 +0100

    78% computed properties & observers are amazing!

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['show'],
-  possibleStates: ['borrowed', 'returned']
+  possibleStates: ['borrowed', 'returned'],
+  contentDidChange: function() {
+    console.log('Called when we add or remove an article');
+  }.observes('model.[]'),
+  stateDidChange: function() {
+    console.log('Called when state changed on article');
+  }.observes('model.@each.state')
 });
--- app/models/friend.js
+++ app/models/friend.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-import Ember from 'ember';
+//import Ember from 'ember';
 
 export default DS.Model.extend({
   articles: DS.hasMany('article', { async: true }),
@@ -8,7 +8,27 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   twitter: DS.attr('string'),
   totalArticles: DS.attr('number'),
-  fullName: Ember.computed('firstName', 'lastName', function() {
-    return this.get('firstName') + ' ' + this.get('lastName');
-  })
+  fullName: function(key,value,oldValue) {
+
+    // as getter:
+    if (arguments.length === 1) {
+      return this.get('firstName') + ' ' + this.get('lastName');
+
+    // as setter:
+    // see book loc 2660
+    // Probably shouldn't do this, though, ref: https://github.com/emberjs/ember.js/issues/9290
+    } else {
+      var name = value.split(' ');
+
+      this.set('firstName', name[0]);
+      this.set('lastName', name[1]);
+
+      return value;
+    }
+
+  }.property('firstName', 'lastName'),
+  // Alternative syntax:
+  // fullName: Ember.computed('firstName', 'lastName', function() {
+  //   return this.get('firstName') + ' ' + this.get('lastName');
+  // })
 });
